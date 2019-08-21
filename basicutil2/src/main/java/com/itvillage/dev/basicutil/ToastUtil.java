cd2f537b1d00dfe2322f7299df5f95207f11b572
Monirozzaman Roni <eproni29@gmail.com>
2019-08-21 15:43:47 +0600

    first commit

---
+++ basicutil2/src/main/java/com/itvillage/dev/basicutil/ToastUtil.java
@@ -0,0 +1,15 @@
+package com.itvillage.dev.basicutil;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by monirozzamanroni on 8/21/2019.
+ */
+
+public class ToastUtil {
+    public static void show(Context context,String message)
+    {
+        Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+    }
+}
